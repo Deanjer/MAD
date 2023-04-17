7472823d163d05ef2f7009994cef093186a79bde
Deanjer <220509@student.glu.nl>
2023-04-17 15:04:38 +0200

    push_test14

--- public/script.js
+++ public/script.js
@@ -23,3 +23,30 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+//modal 2
+
+// Get the second modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the second modal
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the second modal
+var span2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the second modal
+btn2.onclick = function() {
+modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the second modal
+span2.onclick = function() {
+modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the second modal, close it
+window.onclick = function(event) {
+if (event.target == modal2) {
+modal2.style.display = "none";
+}
+}
