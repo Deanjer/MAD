6a5ea088dfbe7408379331efe73a91f9d2b17abd
Deanjer <220509@student.glu.nl>
2023-04-17 15:15:49 +0200

    push_test15

--- public/script.js
+++ public/script.js
@@ -50,3 +50,30 @@ if (event.target == modal2) {
 modal2.style.display = "none";
 }
 }
+// derde modal
+
+// Get the third modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the third modal
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the third modal
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks the button, open the third modal
+btn3.onclick = function() {
+modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the third modal
+span3.onclick = function() {
+modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the third modal, close it
+window.onclick = function(event) {
+if (event.target == modal3) {
+modal3.style.display = "none";
+}
+}
